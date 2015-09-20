f70eeec3ef9d5d6fd71bab313cd44a134b3d5281
landroni <landronimirc@gmail.com>
2015-09-20 22:14:33 +0200

    define global '..values'

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,6 @@
 globalVariables(c('top', 'buttonsFrame', 'typeFrame', 'typeVariable', 
                   'caption.locFrame', 'caption.locVariable', 
                   'caption.placeFrame', 'caption.placeVariable',
-                  'tab.envFrame', 'tab.envVariable'))
+                  'tab.envFrame', 'tab.envVariable', 
+                  '..values'
+                  ))
