184a4759fab351b04202399e9acd40512c884bb6
landroni <landronimirc@gmail.com>
2015-09-20 18:38:52 +0200

    define several other globals

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,4 @@
-globalVariables(c('top', 'buttonsFrame', 'typeFrame', 'typeVariable'))
+globalVariables(c('top', 'buttonsFrame', 'typeFrame', 'typeVariable', 
+                  'caption.locFrame', 'caption.locVariable', 
+                  'caption.placeFrame', 'caption.placeVariable',
+                  'tab.envFrame', 'tab.envVariable'))
